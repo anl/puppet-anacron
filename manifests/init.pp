4f49a4732d9800c9d6cac5a15874b2e75b4cb167
Andrew Leonard <andy@hurricane-ridge.com>
2013-11-02 06:46:01 -0700

    Add programatic configuration of anacrontab.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,15 +4,41 @@
 #
 # === Parameters
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*default_jobs*]
+#   Include OS-installed default jobs in anacrontab.  Default: true
+#
+# [*jobs*]
+#   Hash of jobs to write to anacrontab.  Keys are job-identifiers;
+#   values are hashes with the following keys:
+#   - command: Command to execute
+#   - comment: Comment to be included above job line in anacrontab;
+#   optional.
+#   - delay:   Minutes to delay job start; must be integer
+#   - period:  Frequency of job runs; must be integer or "@monthly".
 #
 # === Examples
 #
-#  class { anacron:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# class { 'anacron':
+#   jobs => {
+#     'duplicity_daily' => {
+#       command => '/opt/duplicity/bin/daily-backup',
+#       delay   => 60,
+#       period  => 1,
+#     },
+#     'zfs_daily_snap' => {
+#       command => '/usr/local/bin/zfs-snapshot.sh tank daily 15',
+#       comment => 'ZFS daily snapshot',
+#       delay   => 15,
+#       period  => 1,
+#     },
+#     'zfs_monthly_snap' => {
+#       command => '/usr/local/bin/zfs-snapshot.sh tank monthly 13',
+#       comment => 'ZFS monthly snapshot',
+#       delay   => 30,
+#       period  => '@monthly',
+#     },
+#   }
+# }
 #
 # === Authors
 #
@@ -22,7 +48,10 @@
 #
 # Copyright 2013 Andrew Leonard
 #
-class anacron inherits anacron::params {
+class anacron (
+  $default_jobs = true,
+  $jobs = {}
+  ) inherits anacron::params {
 
   package { $anacron::params::package:
     ensure => present,
--- vagrant/vagrant.pp
+++ vagrant/vagrant.pp
@@ -1,2 +1,22 @@
-# Include this module
-include anacron
+# Include this class:
+class { 'anacron':
+  jobs => {
+    'duplicity_daily'  => {
+      command => '/opt/duplicity/bin/daily-backup',
+      delay   => 60,
+      period  => 1,
+    },
+    'zfs_daily_snap'   => {
+      command => '/usr/local/bin/zfs-snapshot.sh tank daily 15',
+      comment => 'ZFS daily snapshot',
+      delay   => 15,
+      period  => 1,
+    },
+    'zfs_monthly_snap' => {
+      command => '/usr/local/bin/zfs-snapshot.sh tank monthly 13',
+      comment => 'ZFS monthly snapshot',
+      delay   => 30,
+      period  => '@monthly',
+    },
+  }
+}
